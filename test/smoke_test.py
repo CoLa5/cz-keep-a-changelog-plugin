a45ae356cea828d677ab7f86589a4cb0f94fdc82
Corvin Lasogga <c.lasogga@gmail.com>
2026-06-01 13:36:05 +0200

    ci: Setup ci

---
+++ test/smoke_test.py
@@ -0,0 +1,8 @@
+"""Check that basic features work.
+
+Catch cases where e.g. files are missing so the import doesn't work. It is
+recommended to check that e.g. assets are included."""
+
+import cz_keep_a_changelog_plugin  # noqa: F401
+
+print("Basic import does work")
